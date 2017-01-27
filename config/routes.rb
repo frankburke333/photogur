897a2dc78017b0a16a38487422b89bdc11254cbe
frankburke333 <frank.burke@gmail.com>
2017-01-27 17:02:51 -0500

    completed execise

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
 
   get 'pictures' => 'pictures#index'
+  get 'pictures/new' => 'pictures#new' # this is also a new line of code
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
 
   post 'pictures' => 'pictures#create' # this is a new line of code
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   patch 'pictures/:id' => "pictures#update"
-
   delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
 
-  root 'pictures#index'
+  resources :pictures
+
 
 end
