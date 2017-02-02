9151efad1247af767a5f95d55473f8507d014c29
frankburke333 <frank.burke@gmail.com>
2017-02-02 16:51:03 -0500

    add completed photogur exercise

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,16 @@
 class Picture < ApplicationRecord
 
+
+  validates :artist, presence: { message: "Name is required" }
+
+  #
+  validates :title, length: { minimum: 3, maximum: 20 }
+  #
+  #
+  validates :url, presence:true, uniqueness: true
+
+  validates :url, :format => URI::regexp(%w(http https))
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Picture.create(
-  title:  'The old church on the coast of White sea',
-  artist: 'Sergey Ershov',
-  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg'
+  title:  'The Dunk',
+  artist: 'John Starks',
+  url:    'http://cdn.newsday.com/polopoly_fs/1.3190130.1316645604!/httpImage/image.jpg_gen/derivatives/display_600/image.jpg'
 )
 
 Picture.create(
-  title:  'Sea Power',
-  artist: 'Stephen Scullion',
-  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+  title:  'The Actor',
+  artist: 'Calculon',
+  url:    'http://vignette1.wikia.nocookie.net/es.futurama/images/a/a9/Calculon.png/revision/latest?cb=20130124023257'
 )
 
 Picture.create(
-  title:  'Into the Poppies',
-  artist: 'John Wilhelm',
-  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+  title:  'Burke',
+  artist: 'Frank Burke',
+  url:    'https://s-media-cache-ak0.pinimg.com/originals/36/72/dd/3672dd7f883e0d2e9f8655978cacc84f.jpg'
 )
